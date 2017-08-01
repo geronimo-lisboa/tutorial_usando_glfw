d9444a6c433b8590d6b9e8a44f35225b9eecbbe6
luciano <luciano.geronimo.fnord@gmail.com>
2017-08-01 13:24:34 -0300

    classe do shader

---
+++ shader.h
@@ -0,0 +1,26 @@
+﻿#include <GL\glew.h>
+#include <map>
+#include <string>
+#include <sstream>
+
+using namespace std;
+class Shader
+{
+private:
+	GLuint programId, vsId, fsId;
+	map<string, GLuint> attributes;
+	map<string, GLuint> uniforms;
+	std::string ReadShaderFile(std::string path);
+	void introspectProgram(GLuint programId, map<string, GLuint> &attributes, map<string, GLuint> &uniforms);
+	string GetShaderInfoLog(GLuint object, PFNGLGETSHADERIVPROC glGet__iv, PFNGLGETSHADERINFOLOGPROC glGet__InfoLog);
+	GLuint MakeShader(GLenum type, std::string source);
+	GLuint MakeProgram(GLuint vertex_shader, GLuint fragment_shader);
+public:
+	Shader(string vsPath, string fsPath);
+	Shader(stringstream vsStream, stringstream fsStream);
+	~Shader();
+	GLuint GetUniform(string name);
+	GLuint GetAttribute(string name);
+	GLuint GetProgramId();
+	void UseProgram();
+};
